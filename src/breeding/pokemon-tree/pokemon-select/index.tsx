3c0aab31a239df07667f272d0f1c943fe7fac17f
greidinger-reis <jp.greidinger@hotmail.com>
2023-08-31 12:02:41 -0300

    up next version, moved component to a new file

--- src/breeding/pokemon-tree/pokemon-select/index.tsx
+++ src/breeding/pokemon-tree/pokemon-select/index.tsx
@@ -2,8 +2,6 @@
 import { Button } from "@/components/ui/button"
 
 import type { getPokemonByName as getPokemonByNameFunc } from "@/actions/pokemon-by-name"
-import { Female, Male } from "@/components/icons"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import {
   Command,
   CommandEmpty,
@@ -22,26 +20,16 @@ import { Switch } from "@/components/ui/switch"
 import { usePokemonToBreed } from "@/context/hooks"
 import { IV } from "@/context/types"
 import { Pokemon, PokemonSelectList } from "@/data/types"
-import {
-  camelToSpacedPascal,
-  getSprite,
-  isEven,
-  parseNames,
-  raise,
-  randomString,
-} from "@/lib/utils"
-import { HelpCircle, Loader } from "lucide-react"
+import { getSprite, isEven, parseNames, raise, randomString } from "@/lib/utils"
+import { Loader } from "lucide-react"
 import { For, block } from "million/react"
 import React from "react"
-import { Gender } from "../consts"
-import type { BreedNode, GenderType, Position } from "../types"
-import type { useBreedMap } from "../use-breed-map"
-import {
-  GenderlessPokemonEvolutionTree,
-  getBreedPartnerPosition as getBreedingPartnerPosition,
-} from "../utils"
-import { Color, ColorMap } from "./iv-colors"
-import { HeldItemsView } from "./held-items"
+import { Gender } from "../../consts"
+import type { BreedNode, GenderType, Position } from "../../types"
+import type { useBreedMap } from "../../use-breed-map"
+import { GenderlessPokemonEvolutionTree } from "../../utils"
+import { Color, ColorMap } from "../iv-colors"
+import { CurrentNodeInformationCard } from "./current-node-information-card"
 
 const NodeScaleByColorAmount = {
   "5": 1,
@@ -305,106 +293,3 @@ export const PokemonSelect = block(
     )
   },
 )
-
-class BreedPartnerIsNatureOnly {}
-
-//TODO: Improve the UI on this.
-function CurrentNodeInformationCard(props: {
-  currentNode: BreedNode
-  gender: GenderType | null
-  setGender: (gender: GenderType) => void
-  position: Position
-  breedMap: ReturnType<typeof useBreedMap>
-}) {
-  function onCheckedChange(value: boolean) {
-    props.setGender(value ? Gender.FEMALE : Gender.MALE)
-  }
-
-  function getIVDifferenceFromBreedPartner(
-    self: Array<IV>,
-    breedPartner: Array<IV> | BreedPartnerIsNatureOnly,
-  ): IV {
-    if (breedPartner instanceof BreedPartnerIsNatureOnly) {
-      //This means that self is one iv only, so get the first element is fine
-      return self[0]
-    }
-
-    const ivThatDoesntExistOnBreedPartner = self.filter(
-      (iv) => !breedPartner.includes(iv),
-    )
-
-    return ivThatDoesntExistOnBreedPartner[0]
-  }
-
-  const breedPartnerPos =
-    props.position === "0,0" ? null : getBreedingPartnerPosition(props.position)
-  const IVsFromBreedPartner = breedPartnerPos
-    ? props.breedMap.get(breedPartnerPos)?.ivs ?? new BreedPartnerIsNatureOnly()
-    : null
-
-  const ivDifferenceFromBreedPartner =
-    IVsFromBreedPartner &&
-    props.currentNode.ivs &&
-    getIVDifferenceFromBreedPartner(props.currentNode.ivs, IVsFromBreedPartner)
-
-  return (
-    <Card className="w-fit h-fit relative">
-      <CardHeader className="pb-2 pt-4">
-        <HeldItemsView
-          item={
-            //if not natured, ivs must exist.
-            props.currentNode.nature ? "nature" : ivDifferenceFromBreedPartner!
-          }
-        />
-        <CardTitle className="flex items-center">
-          {props.currentNode.pokemon ? (
-            <React.Fragment>
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                src={getSprite(props.currentNode.pokemon.name)}
-                style={{
-                  imageRendering: "pixelated",
-                }}
-                alt={props.currentNode.pokemon.name}
-                className="mb-1"
-              />
-              {props.currentNode.pokemon.name}
-            </React.Fragment>
-          ) : (
-            <HelpCircle size={32} />
-          )}
-        </CardTitle>
-      </CardHeader>
-      <CardContent className="gap-4 flex flex-col">
-        <div className="flex flex-col gap-1">
-          {Boolean(props.currentNode.ivs) ? <p>Ivs:</p> : null}
-          {props.currentNode.ivs?.map((iv) => (
-            <span key={randomString(4)}>31 {camelToSpacedPascal(iv)}</span>
-          ))}
-        </div>
-        {props.currentNode.nature && (
-          <i className="block">{props.currentNode.nature}</i>
-        )}
-        {props.currentNode.pokemon ? (
-          <React.Fragment>
-            <div className="flex flex-col gap-1">
-              <p>Egg Groups:</p>
-              {props.currentNode.pokemon.eggTypes.map((egg) => (
-                <span key={randomString(3)}>{egg}</span>
-              ))}
-            </div>
-            <div className="flex gap-2">
-              <Male className="fill-blue-500 h-6 w-fit" />
-              <Switch
-                className="data-[state=unchecked]:bg-primary"
-                checked={props.gender === Gender.FEMALE}
-                onCheckedChange={onCheckedChange}
-              />
-              <Female className="fill-pink-500 h-6 w-fit -ml-1" />
-            </div>
-          </React.Fragment>
-        ) : null}
-      </CardContent>
-    </Card>
-  )
-}
