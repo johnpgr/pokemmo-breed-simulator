f6483c8ef9a97b7dce2ee673e091444e08386de9
johnpgr <jp.greidinger@hotmail.com>
2024-05-01 18:35:36 -0300

    doesnt work :/

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
 import { PokemonToBreedSelect } from "@/components/PokemonBreedSelect"
 import { PokemonBreedTree } from "@/components/PokemonBreedTree"
 import { PokemonToBreedContext } from "@/components/PokemonToBreedContext"
-import { PokemonSpeciesUnparsed } from "@/core/pokemon"
-import { getBaseUrl } from "@/lib/url"
+import pokemons from "@/data/data.json" assert {type: "json"}
 
 export default async function HomePage() {
-    //TODO: Check if this shait is really getting cached
-    const pokemons = await fetch(`${getBaseUrl()}/data.json`, { cache: "force-cache" }).then(
-        (res) => res.json() as Promise<PokemonSpeciesUnparsed[]>,
-    )
 
     return (
         <PokemonToBreedContext>
--- src/lib/url.ts
+++
@@ -1,3 +0,0 @@
-export function getBaseUrl(): string {
-    return process.env.NODE_ENV === "development" ? "http://localhost:3000" : `https://${process.env.VERCEL_URL}` as string
-}
