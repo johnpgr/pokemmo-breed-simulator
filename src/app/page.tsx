ec1b17dfad51e6cbd058fc17fdcd205fda3fbae5
johnpgr <jp.greidinger@hotmail.com>
2024-09-05 19:03:18 -0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import { BreedContextProvider } from "@/core/PokemonBreedContext"
 import pokemons from "@/data/monster.json" assert { type: "json" }
 import evolutions from "@/data/evolutions.json" assert { type: "json" }
 
+export const runtime = "edge"
+export const dynamic = "force-static"
+
 export default function HomePage() {
     return (
         <BreedContextProvider species={pokemons} evolutions={evolutions}>
