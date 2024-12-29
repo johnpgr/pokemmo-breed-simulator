13ad7124b6c91ac638fff855cdbddc8dddb5ef2c
johnpgr <jp.greidinger@hotmail.com>
2024-12-29 03:43:48 -0300

    Add million lint

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
+import MillionLint from "@million/lint";
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-
     reactStrictMode: true,
     experimental: {
         reactCompiler: true,
     },
 }
 
-export default nextConfig
+export default MillionLint.next({
+    enabled: true,
+    rsc: true
+})(nextConfig);
